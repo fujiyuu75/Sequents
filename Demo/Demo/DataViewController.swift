302b335c26cc66a240e82522fffb6050cea11d0b
fujiyuu75 <fujiyuu75@gmail.com>
2017-06-03 13:28:41 +0900

    Add files

---
+++ Demo/Demo/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Demo
+//
+//  Created by 藤川 裕一 on 2017/05/30.
+//  Copyright © 2017 藤川 裕一. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Sequents/Sequents.swift
@@ -0,0 +1,13 @@
+//
+//  Sequents.swift
+//  Sequents
+//
+//  Created by 藤川 裕一 on 2017/05/30.
+//
+//
+
+import Foundation
+
+open class Sequents {
+    open var isSequent = true
+}
