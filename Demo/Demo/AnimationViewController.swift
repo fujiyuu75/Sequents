c2cb3117cc9ddc15e6970d2e691b8f88e2103238
fujiyuu75 <fujiyuu75@gmail.com>
2017-06-15 08:30:06 +0900

    Add picker

--- Demo/Demo/AnimationViewController.swift
+++ Demo/Demo/AnimationViewController.swift
@@ -8,29 +8,39 @@
 
 import UIKit
 
-class AnimationViewController: UIViewController {
+class AnimationViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var picker: UIPickerView!
+
+    var list = [["fade", "rotate", "bounce"]]
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return list.count
+    }
+
+    func pickerView(_ namePickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list[component].count
+    }
+    //表示する文字列を指定する
+    //PickerViewに表示する配列の要素数を設定する
+    func pickerView(_ namePickerview: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[component][row]
+    }
+
+    //ラベル表示
+    func pickerView(namePickerview: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+//        nameLabel.text = list[row]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        self.view.backgroundColor = .red
+        picker.dataSource = self
+        picker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
